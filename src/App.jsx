1ad92c451cf673e0b5ba2d20dd6e07b5fccb701a
ryankrdh <ryankrdh@gmail.com>
2023-07-21 12:04:51 -0700

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import './styles.css';
+
+// export default makes this main thing exported from this file.
+// when other files want to use this funciton, they can import it like: import App from './App';
+export default function App() {
+  return 'Hi';
+}
