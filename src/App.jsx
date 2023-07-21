992d2bb6af3158232f2f0cec6b4cd5513264894e
ryankrdh <ryankrdh@gmail.com>
2023-07-21 13:16:16 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,46 @@
+import { useState } from 'react';
 import './styles.css';
 
-// export default makes this main thing exported from this file.
-// when other files want to use this funciton, they can import it like: import App from './App';
 export default function App() {
-  return 'Hi';
+  const [newItem, setNewItem] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit} className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input
+            value={newItem}
+            //onInput: handle real-time inputs. onChange will handle the input after comitted by the user
+            onChange={(e) => setNewItem(e.target.value)}
+            type="text"
+            id="item"
+          />
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      <h1 className="header">Todo List</h1>
+      <ul className="list">
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 2
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      </ul>
+    </>
+  );
 }
